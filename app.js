490afec9db2f7150a0f941b5b8b1a17372d90bbc
Emma J <codemzy@gmail.com>
2016-01-25 13:21:35 +0000

    Dynamic route working in app.js --> next to move to routes module

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
 var bodyParser = require('body-parser');
-// force use of nodes native query parser module querystring
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/html/index.html');
-});
+// connect to the MongoDB database
+MongoClient.connect('mongodb://localhost:27017/url', function(err, db) {
 
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
 
-app.get('/new/:url', function(request, response){
-    var url = request.params.url;
-    if (url.match(/[\s]/i) || !(url.match(/[\.]/g))) {
-        response.status(404).json({ error: "URL invalid"});
-    }
-    else { 
-        if (!(url.match(/(http:\/\/)/i)) || !(url.match(/(https:\/\/)/i))) {
-            url = "http://" + url;
+    // homepage
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/public/html/index.html');
+    });
+    
+    
+    // add url via dynamic route
+    app.get('/new/:url', function(request, response){
+        var url = request.params.url;
+        var httpProto = request.headers["x-forwarded-proto"];
+        var myDomain = request.headers.host;
+        if (url.match(/[\s]/i) || !(url.match(/[\.]/g))) {
+            response.status(404).json({ error: "URL invalid"});
         }
-        response.json({ original_url: url });
-    }
-});
+        else { 
+            if (!(url.match(/(http:\/\/)/i)) || !(url.match(/(https:\/\/)/i))) {
+                url = "http://" + url;
+            }
+            
+            // check if url is in the database
+            db.collection('short').find({ "long_url": url }).toArray(function(err, docs) {
+                    assert.equal(err, null);
+                    // if not add the url to the database with a short url
+                    if (docs.length == 0) {
+                        // count the number of entries to add the next short url
+                        db.collection('short').find().count(function(err, count) {
+                            assert.equal(err, null);
+                            var shortNum = count+1;
+                            // add the new url to the database and respond
+                            db.collection('short').insert({ "long_url": url, "short_url": shortNum });
+                            response.json({ original_url: url, short_url: httpProto + "://" + myDomain + "/" + shortNum });
+                        });
+                    } 
+                    // if it is in the database then respond with the result
+                    else {
+                        response.json({ original_url: docs[0].long_url, short_url: httpProto + "://" + myDomain + "/" + docs[0].short_url });
+                    }
+                    
+                });
+        }
+    });
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port', app.get('port'));
+    
+    // listen for client connections
+    app.listen(app.get('port'), function() {
+      console.log('Express server listening on port', app.get('port'));
+    });
+
 });
